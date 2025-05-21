dbb7fa055929d8de7408ff6d37f63dec6df108ea
mihai@customeros.ai <mihai@customeros.ai>
2025-05-21 16:44:17 +0300

    prea baiat

---
+++ assets/ts/pages/DemoPageOne.tsx
@@ -0,0 +1,63 @@
+import { Link, router } from '@inertiajs/react';
+import React from 'react';
+import { Button } from './components/Button/Button';
+
+interface Fact {
+  place: string;
+  facts: {
+    key: string;
+    value: string;
+  }[];
+}
+
+export const DemoPageOne = ({ place, facts }: Fact) => {
+  const loadFacts = () => {
+    router.reload({ only: ['facts'] });
+  };
+  const navigateToGroceries = () => {
+    router.visit('/groceries');
+  };
+
+  return (
+    <>
+      <div className="rounded-md shadow flex items-center gap-x-6 bg-gray-900 px-6 py-2.5 sm:px-3.5 mb-8">
+        <p className="text-sm leading-6 text-white">
+          <Link href="/groceries">
+            <span>
+              A <strong className="font-semibold">more advanced demo</strong> can be found here!
+            </span>
+          </Link>
+        </p>
+      </div>
+      <div className="px-4 sm:px-0">
+        <div className="md:flex md:items-center md:justify-between">
+          <div>
+            <h3 className="text-base font-semibold leading-7 text-gray-900">Hello, {place}!</h3>
+            <p className="mt-1 max-w-2xl text-sm leading-6 text-gray-500">
+              To learn more about the world, click the <em>load facts</em> button.
+            </p>
+          </div>
+          <div className="mt-4 flex md:ml-4 md:mt-0">
+            <Button variant="solid" colorScheme="primary" onClick={navigateToGroceries}>
+              Bulangereala
+            </Button>
+          </div>
+        </div>
+      </div>
+      {facts && (
+        <div className="mt-6 border-t border-gray-100">
+          <dl className="divide-y divide-gray-100">
+            {facts.map(fact => (
+              <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0" key={fact.key}>
+                <dt className="text-sm font-medium leading-6 text-gray-900">{fact.key}</dt>
+                <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+                  {fact.value}
+                </dd>
+              </div>
+            ))}
+          </dl>
+        </div>
+      )}
+    </>
+  );
+};
--- assets/ts/pages/NewGrocery.tsx
+++ assets/ts/pages/NewGrocery.tsx
@@ -1,11 +1,16 @@
-import React from "react";
-import { Link, useForm } from "@inertiajs/react";
+import React from 'react';
+import { Link, useForm } from '@inertiajs/react';
 
-export default function NewGrocery() {
-  function submit(e) {
+export const NewGrocery = () => {
+  const { data, setData, post, processing, errors } = useForm({
+    name: '',
+    quantity: '',
+  });
+
+  const submit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    post("/groceries");
-  }
+    post('/groceries');
+  };
 
   return (
     <form onSubmit={submit}>
@@ -17,19 +22,14 @@ export default function NewGrocery() {
           ← Back to List
         </Link>
         <div className="pb-12">
-          <h2 className="text-base font-semibold leading-7 text-gray-900">
-            New Grocery
-          </h2>
+          <h2 className="text-base font-semibold leading-7 text-gray-900">New Grocery</h2>
           <p className="mt-1 text-sm leading-6 text-gray-600">
             Create a record of a grocery item you need to buy.
           </p>
 
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div className="sm:col-span-4">
-              <label
-                htmlFor="name"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
+              <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">
                 Name
               </label>
 
@@ -37,15 +37,13 @@ export default function NewGrocery() {
                 <input
                   type="text"
                   value={data.name}
-                  onChange={(e) => setData("name", e.target.value)}
+                  onChange={e => setData('name', e.target.value)}
                   id="name"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
 
-              {errors?.name && (
-                <p className="mt-2 text-sm text-red-600">{errors.name}</p>
-              )}
+              {errors?.name && <p className="mt-2 text-sm text-red-600">{errors.name}</p>}
             </div>
             <div className="sm:col-span-2">
               <label
@@ -59,14 +57,12 @@ export default function NewGrocery() {
                 <input
                   type="number"
                   value={data.quantity}
-                  onChange={(e) => setData("quantity", e.target.value)}
+                  onChange={e => setData('quantity', e.target.value)}
                   id="quantity"
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
-              {errors?.quantity && (
-                <p className="mt-2 text-sm text-red-600">{errors.quantity}</p>
-              )}
+              {errors?.quantity && <p className="mt-2 text-sm text-red-600">{errors.quantity}</p>}
             </div>
           </div>
         </div>
@@ -76,14 +72,14 @@ export default function NewGrocery() {
         <button
           type="submit"
           disabled={processing}
-          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
         >
           Submit
         </button>
       </div>
     </form>
   );
-}
+};
 
 // <form onSubmit={submit} className="flex flex-col max-w-md">
 //   <label>Name</label>
