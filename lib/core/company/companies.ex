a83077f111fca56bdd656a9f1a39298aa3444f43
alexopenline <alex@openline.ai>
2025-05-21 21:32:32 +0300

    fix compile issue

--- lib/core/company/companies.ex
+++ lib/core/company/companies.ex
@@ -1,12 +1,13 @@
 defmodule Core.Company.Companies do
-  import Ecto.Query
   alias Core.Repo
   alias Core.Utils.IdGenerator
   alias Core.Company.Schemas.Company
 
-  @spec create_with_domain(any()) :: {:ok, Company.t()} | {:error, Ecto.Changeset.t() | String.t()}
+  @spec create_with_domain(any()) ::
+          {:ok, Company.t()} | {:error, Ecto.Changeset.t() | String.t()}
   def create_with_domain(domain) when not is_binary(domain),
     do: {:error, "domain must be a string"}
+
   def create_with_domain(domain) when is_binary(domain) do
     now = DateTime.utc_now() |> DateTime.truncate(:second)
 
@@ -20,6 +21,7 @@ defmodule Core.Company.Companies do
 
   @spec get_by_domain(any()) :: Company.t() | nil
   def get_by_domain(domain) when not is_binary(domain), do: nil
+
   def get_by_domain(domain) when is_binary(domain) do
     Repo.get_by(Company, primary_domain: domain)
   end
