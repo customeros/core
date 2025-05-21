badb637a8a8b817e5ed291e0f29585919a76489e
Mihai <163020214+umihai29@users.noreply.github.com>
2025-05-21 08:04:53 +0300

    add react in pheonix (#17)

---
+++ lib/web/controllers/page_controller.ex
@@ -0,0 +1,16 @@
+defmodule Web.PageController do
+    use Web, :controller
+  
+    def home(conn, _params) do
+      conn
+      |> assign(:flash, %{})
+      |> assign_prop(:place, "CustomerOs")
+      |> assign_prop(:facts, [
+        %{key: "Population", value: "8 billion"},
+        %{key: "Countries", value: "195"},
+        %{key: "Languages", value: "7,000+"}
+      ])
+      |> render_inertia("DemoPageOne", layout: {Web.Layouts, :app})
+    end
+  end
+  
