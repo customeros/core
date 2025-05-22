301c60f557f13f6118d2d2371a54605e50dbaff5
matt brown <matt@customeros.ai>
2025-05-22 12:30:54 +0100

    waterfall scraper sources

--- lib/core/scraper/scrape.ex
+++ lib/core/scraper/scrape.ex
@@ -1,29 +1,26 @@
 defmodule Core.Scraper.Scrape do
-  @spec scrape_webpage_with_jina(String.t()) ::
+  @spec scrape_webpage(String.t()) ::
           {:ok, String.t(), [String.t()]} | {:error, String.t()}
-  def scrape_webpage_with_jina(url) do
+
+  def scrape_webpage(url) do
     case Core.External.Jina.Service.fetch_page(url) do
       {:ok, content} ->
-        clean_content = Core.Scraper.Clean.process_markdown_webpage(content)
-        links = Core.Scraper.Links.extract_links(clean_content)
-        {:ok, clean_content, links}
+        process_webpage(content)
 
-      {:error, reason} ->
-        {:error, reason}
+      {:error, _reason} ->
+        case Core.External.Puremd.Service.fetch_page(url) do
+          {:ok, content} ->
+            process_webpage(content)
+
+          {:error, reason} ->
+            {:error, reason}
+        end
     end
   end
 
-  @spec scrape_webpage_with_puremd(String.t()) ::
-          {:ok, String.t(), [String.t()]} | {:error, String.t()}
-  def scrape_webpage_with_puremd(url) do
-    case Core.External.Puremd.Service.fetch_page(url) do
-      {:ok, content} ->
-        clean_content = Core.Scraper.Clean.process_markdown_webpage(content)
-        links = Core.Scraper.Links.extract_links(clean_content)
-        {:ok, clean_content, links}
-
-      {:error, reason} ->
-        {:error, reason}
-    end
+  defp process_webpage(content) do
+    clean_content = Core.Scraper.Clean.process_markdown_webpage(content)
+    links = Core.Scraper.Links.extract_links(clean_content)
+    {:ok, clean_content, links}
   end
 end
