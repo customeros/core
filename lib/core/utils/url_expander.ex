b6d533ddb84f3ad7be30d4d93768b112927cbb5a
Matt Brown <113338429+mattbr0wn@users.noreply.github.com>
2025-05-22 11:31:55 +0100

    fix url expander (#46)

--- lib/core/utils/url_expander.ex
+++ lib/core/utils/url_expander.ex
@@ -3,28 +3,28 @@ defmodule Core.Utils.UrlExpander do
   Expands shortened URLs to their full domain.
   Returns {expanded_domain, was_expanded}.
   """
-  def expand_short_url(domain) do
-    url_shorteners = [
-      "bit.ly/",
-      "hubs.ly/"
-    ]
+    def expand_short_url(domain) do
+  url_shorteners = [
+    "bit.ly/",
+    "hubs.ly/"
+  ]
 
-    shortener_found =
-      Enum.any?(url_shorteners, fn shortener ->
-        String.contains?(domain, shortener)
-      end)
+  shortener_found =
+    Enum.any?(url_shorteners, fn shortener ->
+      String.contains?(domain, shortener)
+    end)
 
-    if shortener_found do
-      {has_redirect, expanded_domain} =
-        Core.Utilities.PrimaryDomain.domain_redirect_check(domain)
-
-      if has_redirect do
-        {expanded_domain, true}
-      else
-        {domain, false}
-      end
+  if shortener_found do
+    {is_primary, expanded_domain} = Core.Utils.PrimaryDomain.primary_domain_check(domain)
+    
+    if is_primary && expanded_domain != "" do
+      {expanded_domain, true}
     else
       {domain, false}
     end
+  else
+    {domain, false}
   end
+end
+
 end
