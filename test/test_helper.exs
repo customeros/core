edd9aa25bb8feb8622903a88bf16370fbadbcc00
Mihai <163020214+umihai29@users.noreply.github.com>
2025-05-23 09:54:00 +0300

    fix tests (#75)

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -8,11 +8,25 @@ Mox.defmock(FinchMock, for: Finch.Behaviour)
 Application.put_env(:core, :finch, FinchMock)
 
 # Define mocks for services
-Mox.defmock(Core.External.Jina.Service.Mock, for: Core.External.Jina.Service.Behaviour)
-Mox.defmock(Core.External.Puremd.Service.Mock, for: Core.External.Puremd.Service.Behaviour)
-Mox.defmock(Core.Scraper.Repository.Mock, for: Core.Scraper.Repository.Behaviour)
-Mox.defmock(Core.Ai.Webpage.Classify.Mock, for: Core.Ai.Webpage.Classify.Behaviour)
-Mox.defmock(Core.Ai.Webpage.ProfileIntent.Mock, for: Core.Ai.Webpage.ProfileIntent.Behaviour)
+Mox.defmock(Core.External.Jina.Service.Mock,
+  for: Core.External.Jina.Service.Behaviour
+)
+
+Mox.defmock(Core.External.Puremd.Service.Mock,
+  for: Core.External.Puremd.Service.Behaviour
+)
+
+Mox.defmock(Core.Scraper.Repository.Mock,
+  for: Core.Scraper.Repository.Behaviour
+)
+
+Mox.defmock(Core.Ai.Webpage.Classify.Mock,
+  for: Core.Ai.Webpage.Classify.Behaviour
+)
+
+Mox.defmock(Core.Ai.Webpage.ProfileIntent.Mock,
+  for: Core.Ai.Webpage.ProfileIntent.Behaviour
+)
 
 # Ensure DataCase is loaded
 Code.require_file("test/support/data_case.ex")
