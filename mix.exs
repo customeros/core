fe21f8a405a51b7281b80ab45ec6bfdecb3d35f4
Jonty Knox <jonty@openline.ai>
2025-05-23 09:40:04 +0100

    remove httpoison

--- mix.exs
+++ mix.exs
@@ -78,7 +78,6 @@ defmodule Core.MixProject do
       {:inertia, "~> 2.4.0"},
       {:esbuild, "~> 0.9", runtime: Mix.env() == :dev},
       {:tailwind, "~> 0.3", runtime: Mix.env() == :dev},
-      {:httpoison, "~> 2.0"},
       {:ex_aws, "~> 2.5"},
       {:ex_aws_s3, "~> 2.5"},
       {:sweet_xml, "~> 0.7"}
