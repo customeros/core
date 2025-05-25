d320001af88f746ecc93912bea43e4356b5948e9
Alex Calinica <135133139+acalinica@users.noreply.github.com>
2025-05-25 17:58:36 +0300

    patch: update production configuration to restrict allowed origins to preview environment (#108)

--- config/prod.exs
+++ config/prod.exs
@@ -3,12 +3,7 @@ import Config
 config :core, Web.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
   check_origin: [
-    "https://app.customeros.dev",
-    "https://app.customeros.ai",
-    "https://app.customeros.local",
-    "https://frontera.customeros.ai",
-    "https://frontera.openline.dev",
-    "//*.localcan.dev"
+    "https://preview.customeros.ai"
   ]
 
 config :logger, level: :info
