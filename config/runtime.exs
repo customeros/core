65d8b5a9840c2f4ff3982edc1e764d6152df5c23
alexopenline <alex@openline.ai>
2025-05-26 14:29:37 +0300

    patch: update ex_aws

--- config/runtime.exs
+++ config/runtime.exs
@@ -126,6 +126,10 @@ config :ex_aws, :s3,
     "#{System.get_env("CLOUDFLARE_R2_ACCOUNT_ID")}.r2.cloudflarestorage.com",
   json_codec: Jason
 
+# Configure ExAws to use Finch as the HTTP client
+config :ex_aws, :http_client, ExAws.Request.Finch
+config :ex_aws, :finch_pool, Core.Finch
+
 # Brandfetch configuration
 config :core, :brandfetch, client_id: get_env.("BRANDFETCH_CLIENT_ID", nil)
 
