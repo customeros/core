fccf3d811dba0fff18d188cbaf1df95b1305406f
Alex Calinica <135133139+acalinica@users.noreply.github.com>
2025-05-27 17:50:52 +0300

    feat: enhance TypeScript compilation and add Markdown conversion script (#150)
    - Updated the Compile.Script task to compile multiple TypeScript scripts using Bun.
    - Introduced a new script for converting Markdown to Lexical editor state.
    - Added necessary dependencies for Markdown processing and DOM manipulation.
    - Updated package.json to include new scripts and dependencies.

--- lib/mix/tasks/compile_script.ex
+++ lib/mix/tasks/compile_script.ex
@@ -1,38 +1,55 @@
 defmodule Mix.Tasks.Compile.Script do
   use Mix.Task
 
-  @shortdoc "Compiles the TypeScript script using Bun"
+  @shortdoc "Compiles the TypeScript scripts using Bun"
 
   def run(_) do
     if running_in_elixirls?() do
       Mix.shell().info("Skipping TypeScript compilation in ElixirLS")
     end
 
-    Mix.shell().info("Compiling TypeScript script with Bun...")
+    Mix.shell().info("Compiling TypeScript scripts with Bun...")
 
     # Define script and output paths
     script_dir = Path.expand("scripts")
     output_dir = Path.expand("priv/scripts")
 
-    script_path = Path.join(script_dir, "convert_lexical_to_yjs.ts")
-    output_path = Path.join(output_dir, "convert_lexical_to_yjs")
+    # List of scripts to compile
+    scripts = [
+      "convert_lexical_to_yjs.ts",
+      "convert_md_to_lexical.ts"
+    ]
 
     # Ensure priv/scripts exists
     File.mkdir_p!(output_dir)
 
-    # Run the Bun build command and capture output
-    command = "cd #{script_dir} && bun build #{script_path} --compile --outfile=#{output_path}"
+    # Compile each script
+    Enum.each(scripts, fn script_name ->
+      script_path = Path.join(script_dir, script_name)
 
-    {output, exit_code} = System.cmd("sh", ["-c", command], stderr_to_stdout: true)
+      output_path =
+        Path.join(output_dir, String.replace(script_name, ".ts", ""))
 
-    # Print output for debugging
-    Mix.shell().info(output)
+      # Run the Bun build command and capture output
+      command =
+        "cd #{script_dir} && bun build #{script_path} --compile --outfile=#{output_path}"
 
-    if exit_code == 0 do
-      Mix.shell().info("✅ Successfully compiled script to #{output_path}")
-    else
-      Mix.raise("❌ Failed to compile TS script.\n\nError output:\n#{output}")
-    end
+      {output, exit_code} =
+        System.cmd("sh", ["-c", command], stderr_to_stdout: true)
+
+      # Print output for debugging
+      Mix.shell().info(output)
+
+      if exit_code == 0 do
+        Mix.shell().info(
+          "✅ Successfully compiled #{script_name} to #{output_path}"
+        )
+      else
+        Mix.raise(
+          "❌ Failed to compile #{script_name}.\n\nError output:\n#{output}"
+        )
+      end
+    end)
   end
 
   defp running_in_elixirls? do
