6917a6d145735528ac4a6d1e65b9fb73bc959089
Mihai <163020214+umihai29@users.noreply.github.com>
2025-05-29 12:42:50 +0300

    patch: added colors for the user presence (#178)

--- lib/web/channels/leads_channel.ex
+++ lib/web/channels/leads_channel.ex
@@ -2,9 +2,18 @@ defmodule Web.Channels.LeadsChannel do
   use Web, :channel
   require Logger
   alias Web.Presence
+  alias Core.Auth.Users.ColorManager
 
   @impl true
   def join("leads:" <> tenant_id, params, socket) do
+    {:ok, color} = ColorManager.assign_color(params["user_id"])
+
+    socket =
+      socket
+      |> assign(:user_id, params["user_id"])
+      |> assign(:username, params["username"])
+      |> assign(user_color: %{params["user_id"] => color})
+
     send(self(), {:after_join, tenant_id, params})
     {:ok, socket}
   end
@@ -41,7 +50,8 @@ defmodule Web.Channels.LeadsChannel do
         %{
           username: params["username"],
           online_at: inspect(System.system_time(:second)),
-          metadata: %{"source" => "customerOS"}
+          metadata: %{"source" => "customerOS"},
+          color: Map.get(socket.assigns.user_color, params["user_id"])
         }
       )
 
@@ -51,6 +61,7 @@ defmodule Web.Channels.LeadsChannel do
 
   @impl true
   def terminate(_, socket) do
+    ColorManager.release_color(socket.assigns.user_id)
     {:ok, socket}
   end
 end
