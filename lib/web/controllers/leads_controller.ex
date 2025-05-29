5131d0494c104e2e444208c3b8b2ed3ce149f22e
Alex Calinica <acalinica@customeros.ai>
2025-05-29 14:28:17 +0300

    feat: add OpenTelemetry tracing to leads listing and controller actions
    - Integrated OpenTelemetry tracing in the Leads module's `list_view_by_tenant_id` function to monitor performance and attributes related to tenant ID.
    - Enhanced the LeadsController's `index` action with tracing to capture tenant and user IDs for better observability.
    - Improved the structure of the code for clarity and maintainability.

--- lib/web/controllers/leads_controller.ex
+++ lib/web/controllers/leads_controller.ex
@@ -1,15 +1,23 @@
 defmodule Web.LeadsController do
   use Web, :controller
+  require OpenTelemetry.Tracer
   alias Core.Crm.Leads
   alias CSV
 
   def index(conn, _params) do
-    %{tenant_id: tenant_id} = conn.assigns.current_user
-    companies = Leads.list_view_by_tenant_id(tenant_id)
+    OpenTelemetry.Tracer.with_span "web.leads_controller:index" do
+      OpenTelemetry.Tracer.set_attributes([
+        {"tenant.id", conn.assigns.current_user.tenant_id},
+        {"user.id", conn.assigns.current_user.id}
+      ])
 
-    conn
-    |> assign_prop(:companies, companies)
-    |> render_inertia("Leads")
+      %{tenant_id: tenant_id} = conn.assigns.current_user
+      companies = Leads.list_view_by_tenant_id(tenant_id)
+
+      conn
+      |> assign_prop(:companies, companies)
+      |> render_inertia("Leads")
+    end
   end
 
   def download(conn, _params) do
