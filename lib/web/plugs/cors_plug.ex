2a203f68e80b3b6a63e8d4634776cc1b4f9f7737
Jonty Knox <jonty@openline.ai>
2025-05-30 15:15:17 +0100

    add cors to public api

---
+++ lib/web/plugs/cors_plug.ex
@@ -0,0 +1,39 @@
+defmodule Web.Plugs.CorsPlug do
+  import Plug.Conn
+
+  @allowed_origins [
+    "https://customeros.ai",
+    "https://*.customeros.ai",
+    "https://unreal-world-072818.framer.app"
+  ]
+
+  def init(opts), do: opts
+
+  def call(conn, _opts) do
+    origin = get_req_header(conn, "origin") |> List.first()
+
+    if origin && origin_allowed?(origin) do
+      conn
+      |> put_resp_header("access-control-allow-origin", origin)
+      |> put_resp_header("access-control-allow-methods", "POST, OPTIONS")
+      |> put_resp_header("access-control-allow-headers", "Content-Type, Authorization")
+      |> put_resp_header("access-control-max-age", "86400")
+    else
+      conn
+    end
+  end
+
+  defp origin_allowed?(origin) do
+    uri = URI.parse(origin)
+    host = uri.host || ""
+
+    Enum.any?(@allowed_origins, fn allowed ->
+      case allowed do
+        "https://" <> pattern ->
+          String.match?(host, ~r/^#{String.replace(pattern, "*", ".*")}$/)
+        _ ->
+          false
+      end
+    end)
+  end
+end
