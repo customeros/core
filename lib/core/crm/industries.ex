7e4380fb9c2ca24ecd09dba287505b6e4ff961f8
alexopenline <alex@openline.ai>
2025-06-03 17:46:00 +0300

    patch: add industry mapping from 2017 codes

--- lib/core/crm/industries.ex
+++ lib/core/crm/industries.ex
@@ -11,7 +11,9 @@ defmodule Core.Crm.Industries do
 
   @spec get_by_code(String.t()) :: Industry.t() | nil
   def get_by_code(code) when is_binary(code) do
-    Repo.get_by(Industry, code: code)
+    code
+    |> map_naics_code()
+    |> (&Repo.get_by(Industry, code: &1)).()
   end
 
   def get_by_code(_), do: nil
@@ -21,5 +23,21 @@ defmodule Core.Crm.Industries do
     %Industry{}
     |> Industry.changeset(attrs)
     |> Repo.insert()
+  end
+
+  @doc """
+  Map of old (2017 and before) NAICS codes to new (2022) NAICS codes.
+  """
+  @naics_code_map %{
+    "454110" => "444140",
+    "453210" => "459410"
+  }
+
+  @doc """
+  Returns the new NAICS code if the given code is mapped; otherwise returns the code itself.
+  """
+  @spec map_naics_code(String.t()) :: String.t()
+  def map_naics_code(code) when is_binary(code) do
+    Map.get(@naics_code_map, code, code)
   end
 end
