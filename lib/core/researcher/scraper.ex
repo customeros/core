0b259b1f5431c5deffff8eb7daad285ac1e83477
alexopenline <alex@openline.ai>
2025-06-04 16:12:33 +0300

    patch: add reason error

--- lib/core/researcher/scraper.ex
+++ lib/core/researcher/scraper.ex
@@ -99,7 +99,7 @@ defmodule Core.Researcher.Scraper do
         {:ok, content}
 
       {:error, reason} ->
-        Logger.error("Failed processing scraped content for #{url}")
+        Logger.error("Failed processing scraped content for #{url} | reason: #{inspect(reason)}")
         {:error, reason}
     end
   end
