6f1edb4e19cc3c561ba3444ecd1ffeef95d6ea21
alexopenline <alex@openline.ai>
2025-06-05 20:20:22 +0300

    patch: fix app crash

--- priv/repo/migrations/20250605143855_add_homepage_scraped_to_companies.exs
+++ priv/repo/migrations/20250605143855_add_homepage_scraped_to_companies.exs
@@ -1,17 +1,15 @@
 defmodule Core.Repo.Migrations.AddHomepageScrapedToCompanies do
   use Ecto.Migration
 
-  def change do
-    up do
-      alter table(:companies) do
-        add :homepage_scraped, :boolean, default: false, null: false
-      end
+  def up do
+    alter table(:companies) do
+      add(:homepage_scraped, :boolean, default: false, null: false)
     end
+  end
 
-    down do
-      alter table(:companies) do
-        remove :homepage_scraped
-      end
+  def down do
+    alter table(:companies) do
+      remove(:homepage_scraped)
     end
   end
 end
