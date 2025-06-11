7b69054a991a97618827e104646dcae235aacb75
alexopenline <alex@openline.ai>
2025-06-11 19:14:31 +0300

    patch: fix icp profiles crash

--- lib/core/researcher/icp_profiles.ex
+++ lib/core/researcher/icp_profiles.ex
@@ -55,11 +55,7 @@ defmodule Core.Researcher.IcpProfiles do
 
       case Repo.get_by(Profile, tenant_id: tenant_id) do
         %Profile{} = icp ->
-          {:ok,
-           %{
-             profile: icp.profile,
-             qualifying_attributes: icp.qualifying_attributes
-           }}
+          {:ok, icp}
 
         nil ->
           Tracing.error(:not_found)
