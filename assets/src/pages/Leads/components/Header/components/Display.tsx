0897bfe18276f5f04cfe56abcc428622db5c69e3
Alex Calinica <acalinica@customeros.ai>
2025-06-16 14:33:40 +0300

    patch: enhance leads filtering and display options
    - Added 'none' option to the group state in UrlState for better filtering.
    - Updated Leads component to handle new 'asc' state for sorting leads.
    - Introduced Display component for managing display settings like pipeline visibility, grouping, and ordering.
    - Refactored Pipeline component to use updated props and improve readability.
    - Adjusted backend logic to accommodate new grouping options.

---
+++ assets/src/pages/Leads/components/Header/components/Display.tsx
@@ -0,0 +1,140 @@
+import { UrlState } from 'src/types';
+import { useUrlState } from 'src/hooks';
+import { Icon } from 'src/components/Icon';
+import { Button } from 'src/components/Button';
+import { Toggle } from 'src/components/Toggle';
+import { Select } from 'src/components/Select';
+import { IconButton } from 'src/components/IconButton';
+import { Popover, PopoverContent, PopoverTrigger } from 'src/components/Popover';
+
+const orderByOptions = [
+  { label: 'Created', value: 'inserted_at' },
+  { label: 'Name', value: 'name' },
+  { label: 'Industry', value: 'industry' },
+  { label: 'Stage', value: 'stage' },
+  { label: 'Country', value: 'country' },
+];
+
+export const Display = () => {
+  const { getUrlState, setUrlState } = useUrlState<UrlState>();
+  const { pipeline, group, desc, asc } = getUrlState();
+  const orderBy = desc || asc;
+
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          size="xs"
+          variant="ghost"
+          colorScheme="gray"
+          className="hidden md:flex"
+          leftIcon={<Icon name="distribute-spacing-vertical" />}
+        >
+          Display
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="flex flex-col gap-2 w-[221px] p-3">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-2">
+            <Icon name="recording-01" className="text-gray-500" />
+            <span>Pipeline</span>
+          </div>
+
+          <Toggle
+            size="sm"
+            isChecked={pipeline !== 'hidden'}
+            onChange={value => {
+              setUrlState(params => ({
+                ...params,
+                pipeline: value ? 'visible' : 'hidden',
+              }));
+            }}
+          />
+        </div>
+
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-2">
+            <Icon name="rows-01" className="text-gray-500" />
+            <span>Grouping</span>
+          </div>
+
+          <Toggle
+            size="sm"
+            isChecked={group !== 'none'}
+            onChange={value => {
+              setUrlState(params => ({
+                ...params,
+                group: value ? 'stage' : 'none',
+              }));
+            }}
+          />
+        </div>
+
+        <div className="flex items-center justify-between gap-2">
+          <div className="flex items-center gap-2">
+            <Icon className="text-gray-500" name="arrow-switch-vertical-01" />
+            <span>Ordering</span>
+          </div>
+
+          <div className="w-fit flex items-center gap-2">
+            <Select
+              size="xxs"
+              isSearchable={false}
+              menuWidth="fit-item"
+              placeholder="Order by"
+              options={orderByOptions}
+              value={orderByOptions.find(option => option.value === (orderBy || 'inserted_at'))}
+              onChange={value => {
+                setUrlState(({ desc, asc, ...rest }) => {
+                  if (value.value === desc || value.value === asc) {
+                    return rest;
+                  }
+
+                  return {
+                    ...rest,
+                    desc: value.value,
+                  };
+                });
+              }}
+            />
+
+            <IconButton
+              size="xxs"
+              aria-label="icp"
+              variant="outline"
+              icon={<Icon name={asc ? 'arrows-up' : 'arrows-down'} />}
+              onClick={() => {
+                setUrlState(({ desc, asc, ...rest }) => {
+                  if (desc && asc) {
+                    return {
+                      ...rest,
+                    };
+                  }
+
+                  if (desc) {
+                    return {
+                      ...rest,
+                      asc: desc,
+                    };
+                  }
+
+                  if (asc) {
+                    return {
+                      ...rest,
+                      desc: asc,
+                    };
+                  }
+
+                  return {
+                    ...rest,
+                    asc: 'inserted_at',
+                  };
+                });
+              }}
+            />
+          </div>
+        </div>
+      </PopoverContent>
+    </Popover>
+  );
+};
--- assets/src/pages/Leads/components/Pipeline/Pipeline.tsx
+++ assets/src/pages/Leads/components/Pipeline/Pipeline.tsx
@@ -1,37 +1,30 @@
-import { useEffect } from 'react';
+import type { Stage, UrlState } from 'src/types';
 
 import { cn } from 'src/utils/cn';
 import { useUrlState } from 'src/hooks';
-import { Lead, Stage, UrlState } from 'src/types';
 import { Icon, IconName } from 'src/components/Icon/Icon';
 
 import { stageIcons, stageOptions } from '../util';
 
 interface PipelineProps {
-  max_count: number;
-  scroll_progress: number;
-  stage_counts: Record<Stage, number>;
-  leads: Lead[] | Record<Stage, Lead[]>;
+  maxCount: number;
+  scrollProgress: number;
+  stageCounts: Record<Stage, number>;
   onStageClick: (stage: Stage | null) => void;
 }
 
 export const Pipeline = ({
-  leads,
-  stage_counts,
-  scroll_progress,
-  max_count,
+  stageCounts,
+  scrollProgress,
+  maxCount,
   onStageClick,
 }: PipelineProps) => {
   const { getUrlState } = useUrlState<UrlState>();
   const { pipeline, stage: selectedStage, group } = getUrlState();
 
   const getHeight = (count: number) => {
-    return scroll_progress < 0.2 ? `${count ? (count / max_count) * 100 + 10 : 20}px` : '20px';
+    return scrollProgress < 0.2 ? `${count ? (count / maxCount) * 100 + 10 : 20}px` : '20px';
   };
-
-  useEffect(() => {
-    // ... existing code ...
-  }, [scroll_progress, max_count]);
 
   if (pipeline === 'hidden') {
     return null;
@@ -41,13 +34,13 @@ export const Pipeline = ({
     <div
       className={cn(
         'w-full items-center justify-center mb-2 mt-2 p-1 hidden md:flex max-w-[800px] mx-auto bg-primary-25 rounded-[8px] transition-all duration-200',
-        scroll_progress > 0.2 && 'rounded-md'
+        scrollProgress > 0.2 && 'rounded-md'
       )}
     >
       {stageOptions.map((stage, index) => {
-        const count = stage_counts[stage.value as Stage] || 0;
-        const prevCount = stage_counts[stageOptions[index - 1]?.value as Stage] || 0;
-        const nextCount = stage_counts[stageOptions[index + 1]?.value as Stage] || 0;
+        const count = stageCounts[stage.value as Stage] || 0;
+        const prevCount = stageCounts[stageOptions[index - 1]?.value as Stage] || 0;
+        const nextCount = stageCounts[stageOptions[index + 1]?.value as Stage] || 0;
 
         return (
           <div
@@ -64,16 +57,16 @@ export const Pipeline = ({
             }}
             className={cn(
               'flex-1 flex items-center justify-center bg-primary-100 cursor-pointer hover:bg-primary-200 duration-300',
-              scroll_progress < 0.2 &&
+              scrollProgress < 0.2 &&
                 count > nextCount &&
                 (count - nextCount < 5 ? 'rounded-r-xs' : 'rounded-r-md'),
-              scroll_progress < 0.2 &&
+              scrollProgress < 0.2 &&
                 count > prevCount &&
                 (count - prevCount < 5 ? 'rounded-l-xs' : 'rounded-l-md'),
-              scroll_progress > 0.2 &&
+              scrollProgress > 0.2 &&
                 index === 0 &&
                 (count - nextCount > 5 ? 'rounded-l-xs' : 'rounded-l-md'),
-              scroll_progress > 0.2 &&
+              scrollProgress > 0.2 &&
                 index === stageOptions.length - 1 &&
                 (count - prevCount > 5 ? 'rounded-r-xs' : 'rounded-r-md'),
               selectedStage === stage.value && 'bg-primary-200',
