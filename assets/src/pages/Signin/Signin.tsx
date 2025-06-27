203732a9737c2b50eb012030ce3d2169aaf8f8bb
mihai@customeros.ai <mihai@customeros.ai>
2025-06-27 15:21:31 +0300

    patch:fixes on icp fit decider

--- assets/src/pages/Signin/Signin.tsx
+++ assets/src/pages/Signin/Signin.tsx
@@ -25,6 +25,7 @@ export default function Signin() {
   };
 
   if (errors?.lead === 'Lead still evaluating') {
+    // if (true) {
     return (
       <RootLayout>
         <IcpFitStatus email={data.email} />
@@ -34,106 +35,108 @@ export default function Signin() {
 
   return (
     <RootLayout>
-      <div className="h-screen w-screen flex overflow-hidden max-h-screen max-w-screen">
-        <div className="flex-1 items-center h-screen overflow-hidden">
-          <div className="h-full flex items-center justify-center relative">
-            <div className="relative flex flex-col items-center justify-center w-[768px] h-full px-6 pb-6 bg-white">
-              <div className="h-full flex items-center justify-center relative">
-                <div className="flex flex-col items-center justify-center w-full">
-                  <div className="flex flex-col items-center w-[360px] z-10">
-                    <img width={264} alt="CustomerOS" src="/images/CustomerOs-logo.png" />
-                    {!emailSent && (
-                      <>
-                        <p className="text-3xl font-semibold -mt-[34px] text-gray-900">
-                          Good to see you
-                        </p>
-                      </>
-                    )}
-                  </div>
-                  <form onSubmit={submit} className="flex flex-col items-center gap-4 w-[360px]">
-                    {emailSent ? (
-                      <div className="space-y-2 text-center pt-4 -mt-[60px]">
-                        <p className="font-semibold text-3xl">Check your email</p>
-                        <p>
-                          We've sent you an email with a magic link to{' '}
-                          <span className="font-medium">{data.email}</span>
-                        </p>
-                        <Button
-                          variant="outline"
-                          className="mt-4 w-full"
-                          onClick={() => {
-                            setEmailSent(false);
-                            reset();
-                          }}
-                        >
-                          Back home
-                        </Button>
-                      </div>
-                    ) : (
-                      <>
-                        <p className="text-base mt-3">We'll email you a magic link to get in </p>
-                        <div className="w-full flex flex-col items-start">
-                          <Input
-                            size="md"
-                            type="email"
-                            variant="outline"
-                            value={data.email}
-                            autoComplete="email"
-                            invalid={!!errors.email}
-                            placeholder="Enter your email"
-                            onChange={e => setData('email', e.target.value)}
-                          />
-                          <p
-                            className={cn(
-                              'text-error-500  h-[0px] transition-all mt-0 text-[12px]',
-                              errors.email?.length && 'h-[13px] mt-1',
-                              errors.lead?.length && 'h-[13px] mt-1'
-                            )}
-                          >
-                            {errors?.lead ? errors?.lead : errors?.email}
-                          </p>
-                        </div>
-                        <Button
-                          size="md"
-                          type="submit"
-                          className="w-full"
-                          colorScheme="primary"
-                          isDisabled={processing}
-                        >
-                          Send link
-                        </Button>
-                      </>
-                    )}
+      <div className="h-screen w-screen flex flex-col items-center justify-start overflow-hidden translate-y-[26%]">
+        <div className="flex items-center justify-start flex-col max-w-[768px]">
+          <img
+            width={88}
+            height={88}
+            alt="CustomerOS"
+            src="/images/logo.svg"
+            className=" z-10 object-contain"
+          />
+          <img
+            width={180}
+            height={20}
+            alt="CustomerOS"
+            src="/images/watermark.svg"
+            className="mt-4 z-10 object-contain"
+          />
+        </div>
 
-                    {!emailSent && (
-                      <>
-                        <div className="text-gray-500 text-center text-xs pt-4">
-                          By signing in you agree to CustomerOS's
-                          <div className="text-gray-500">
-                            <a
-                              className="text-primary-700 mr-1 underline"
-                              href="https://customeros.ai/legal/terms-of-service"
-                            >
-                              Terms of Service
-                            </a>
-                            <span className="mr-1">and</span>
-                            <a
-                              className="text-primary-700 underline"
-                              href="https://www.customeros.ai/legal/privacy-policy"
-                            >
-                              Privacy Policy
-                            </a>
-                            .
-                          </div>
-                        </div>
-                      </>
-                    )}
-                  </form>
+        {!emailSent && (
+          <>
+            <p className="text-3xl font-semibold text-gray-900 mt-6">Good to see you</p>
+          </>
+        )}
+        <form onSubmit={submit} className="flex flex-col items-center gap-4 w-[360px]">
+          {emailSent ? (
+            <div className="space-y-2 text-center pt-4 -mt-[60px]">
+              <p className="font-semibold text-3xl">Check your email</p>
+              <p>
+                We've sent you an email with a magic link to{' '}
+                <span className="font-medium">{data.email}</span>
+              </p>
+              <Button
+                variant="outline"
+                className="mt-4 w-full"
+                onClick={() => {
+                  setEmailSent(false);
+                  reset();
+                }}
+              >
+                Back home
+              </Button>
+            </div>
+          ) : (
+            <>
+              <div className="w-full flex flex-col items-start">
+                <Input
+                  size="md"
+                  type="email"
+                  variant="outline"
+                  value={data.email}
+                  autoComplete="email"
+                  className="mt-4 mb-4"
+                  invalid={!!errors.email}
+                  placeholder="Enter your email"
+                  onChange={e => setData('email', e.target.value)}
+                />
+                <p
+                  className={cn(
+                    'text-error-500  h-[0px] transition-all mt-0 text-[12px]',
+                    errors.email?.length && 'h-[13px] mt-1',
+                    errors.lead?.length && 'h-[13px] mt-1'
+                  )}
+                >
+                  {errors?.lead ? errors?.lead : errors?.email}
+                </p>
+              </div>
+              <Button
+                size="md"
+                type="submit"
+                className="w-full"
+                colorScheme="primary"
+                isDisabled={processing}
+              >
+                Next
+              </Button>
+            </>
+          )}
+
+          {!emailSent && (
+            <>
+              <div className="text-gray-500 text-center text-xs pt-2">
+                By signing in you agree to CustomerOS's
+                <div className="text-gray-500">
+                  <a
+                    className="text-primary-700 mr-1 underline"
+                    href="https://customeros.ai/legal/terms-of-service"
+                  >
+                    Terms of Service
+                  </a>
+                  <span className="mr-1">and</span>
+                  <a
+                    className="text-primary-700 underline"
+                    href="https://www.customeros.ai/legal/privacy-policy"
+                  >
+                    Privacy Policy
+                  </a>
+                  .
                 </div>
               </div>
-            </div>
-          </div>
-        </div>
+            </>
+          )}
+        </form>
       </div>
     </RootLayout>
   );
