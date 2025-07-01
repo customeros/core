22a90b56fd2b0832e4874a0865ced7740d2088e4
alexopenline <alex@openline.ai>
2025-07-01 16:30:33 +0300

    patch: process leads

--- priv/repo/migrations/20250701102834_create_table_lead_domain_queues.exs
+++ priv/repo/migrations/20250701102834_create_table_lead_domain_queues.exs
@@ -9,6 +9,8 @@ defmodule Core.Repo.Migrations.CreateTableLeadDomainQueues do
       add :rank, :integer, null: true
       add :processed_at, :utc_datetime, null: true
     end
+
+    create unique_index(:lead_domain_queues, [:tenant_id, :domain], name: :lead_domain_queues_tenant_domain_unique_index)
   end
 
   def down do
