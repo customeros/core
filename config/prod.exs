9e574e9cbf11ea9197d4702361c1bba44aa2662d
Matt Brown <113338429+mattbr0wn@users.noreply.github.com>
2025-07-17 08:57:04 +0100

    patch: add signoz logger backend (#764)

--- config/prod.exs
+++ config/prod.exs
@@ -12,7 +12,15 @@ config :core, :url,
   host: System.get_env("PHX_HOST", "app.customeros.ai")
 
 config :logger,
-  backends: [:console, Core.Notifications.CrashMonitor],
+  backends: [
+    :console, 
+    Core.Notifications.CrashMonitor,
+    {Core.Logger.SignozLogger, [
+      env: System.get_env("OTEL_ENVIRONMENT", "production"),
+      endpoint: System.get_env("SIGNOZ_LOGS_ENDPOINT", "http://10.0.16.2:4318/v1/logs"),
+      service_name: System.get_env("OTEL_SERVICE_NAME", "customeros-core")
+    ]}
+  ],
   level: :warning
 
 config :core, :app_env, :prod
