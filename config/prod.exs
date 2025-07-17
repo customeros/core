8980529c3465a1186844a41e11fc4709f54387d7
matt brown <matt@customeros.ai>
2025-07-17 19:07:14 +0100

    patch: batch log shipping udp

--- config/prod.exs
+++ config/prod.exs
@@ -15,14 +15,14 @@ config :logger,
   backends: [
     :console,
     Core.Notifications.CrashMonitor,
-    {Core.Logger.SignozLogger,
+    {Core.Logger.SignozUdpLogger,
      [
+       host: System.get_env("SIGNOZ_UDP_HOST", "10.0.16.2"),
+       port: String.to_integer(System.get_env("SIGNOZ_UDP_PORT", "54525")),
        env: System.get_env("OTEL_ENVIRONMENT", "production"),
-       endpoint:
-         System.get_env("SIGNOZ_LOGS_ENDPOINT", "http://10.0.16.2:4318/v1/logs"),
        service_name: System.get_env("OTEL_SERVICE_NAME", "customeros-core"),
-       batch_size: 100,
-       batch_timeout: 5_000
+       batch_size: 50,
+       batch_timeout: 2_000
      ]}
   ],
   level: :warning
