1b5ca0a5aa3c52cd042405a083dc2507f594a676
Matt Brown <113338429+mattbr0wn@users.noreply.github.com>
2025-07-17 07:00:36 +0100

    patch: fix paid campaign crash (#762)

--- lib/core/web_tracker/sessions/paid_campaigns.ex
+++ lib/core/web_tracker/sessions/paid_campaigns.ex
@@ -3,6 +3,7 @@ defmodule Core.WebTracker.Sessions.PaidCampaigns do
   alias Core.WebTracker.Sessions.PaidCampaign
 
   @err_not_found {:error, "paid campaign not found"}
+  @err_no_data {:error, "no paid campaign data provided"}
 
   def get_by_hash(hash) do
     case Repo.get_by(PaidCampaign, hash: hash) do
@@ -10,6 +11,8 @@ defmodule Core.WebTracker.Sessions.PaidCampaigns do
       nil -> @err_not_found
     end
   end
+
+  def upsert(nil), do: @err_no_data
 
   def upsert(attrs) when is_map(attrs) do
     changeset = PaidCampaign.changeset(%PaidCampaign{}, attrs)
